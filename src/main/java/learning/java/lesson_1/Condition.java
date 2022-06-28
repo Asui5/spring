85752c8be2747678730f5e8b90e01f187d248dd1
CommoMegaSator <valentyn.vivcharyk@gmail.com>
2022-06-28 15:57:38 +0300

    lesson 1 added new tasks

--- src/main/java/learning/java/lesson_1/Condition.java
+++ src/main/java/learning/java/lesson_1/Condition.java
@@ -3,34 +3,23 @@ package learning.java.lesson_1;
 public class Condition {
 
     public Boolean isPositive(int number){
-        if(number>=0) return true;
+        //ToDo: визначити чи число не від'ємне і повернути true/false
         return false;
     }
 
-    public void oddNumber(){
-        //@ToDo знайти всі непарні числа від 0 до 10 (% ділення з остачею, != не дорівнює, == дорівнює)
-        for (int i=0;i<=10;i++){
-            if(i%2!=0)
-                System.out.println(i);
-        }
+    public int[] oddNumber(){
+        //ToDo: знайти всі непарні числа від 0 до 10 (% ділення з остачею, != не дорівнює, == дорівнює)
+        // і повернути масив цих чисел (заповнивши кожен елемент від нульового)
+        return new int[10];
+    }
 
+    //ToDO: додати метод, що приймає 3 параметра, два числа і один символ,
+    // тоді в залежності від символа виконує дію і повертає результат
+    public double calc(double first, double second, char operation) {
+        return 0.0;
     }
-    public void tenNumber(){
-        for (int i=0;i<10;i++)
-        System.out.println(i);
-    }
-//    public void allNumbers(){
-//        int i=0;
-//        do {
-//            System.out.println(i++);
-//        }while(i<10);
-//    }
 
     public static void main(String[] args) {
-        Condition condition = new Condition();
-        condition.isPositive(1);
-        //condition.oddNumber();
-//        condition.allNumbers();
-        condition.tenNumber();
+        // додати виклики методів або тестити через ConditionTest.java
     }
 }
--- src/test/java/learning/java/lesson_1/ConditionTest.java
+++ src/test/java/learning/java/lesson_1/ConditionTest.java
@@ -3,8 +3,7 @@ package learning.java.lesson_1;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 class ConditionTest {
     private Condition condition;
@@ -27,5 +26,25 @@ class ConditionTest {
     @Test
     void isPositiveZero(){
         assertTrue(condition.isPositive(0));
+    }
+
+    @Test
+    void oddNumberFromZeroToTen() {
+        int count = 0;
+        int[] array = new int[10];
+        for (int i = 0; i < 10; i++) {
+            if (i % 2 != 0) {
+                array[count] = i;
+                count++;
+            }
+        }
+        assertEquals(array, condition.oddNumber());
+    }
+
+    void testCalc() {
+        assertEquals(6, condition.calc(2,4, '+'));
+        assertEquals(5, condition.calc(8,3,'-'));
+        assertEquals(99, condition.calc(9,11,'*'));
+        assertEquals(3, condition.calc(9,3,'/'));
     }
 }
